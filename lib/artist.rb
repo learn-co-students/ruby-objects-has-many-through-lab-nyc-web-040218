f7ea62ca58100ef24afa9b71480246e696c5f96a
William Dale <williambdale@gmail.com>
2018-04-06 15:22:18 -0400

    Add artist, song and genre classes

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+
+  attr_reader :name, :songs, :genres
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @genres = []
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+    @genres << song.genre
+    song.genre.artists << self
+  end
+
+end
